6086e5dc1102eda6818fac3285e7c4853a37114b
PirraToZ <pirratoz@gmail.com>
2024-07-17 19:27:57 +0300

    feat(): add utils for JWT, pass

---
+++ grocery/utils/jwt.py
@@ -0,0 +1,43 @@
+from typing import Any
+from datetime import (
+    datetime,
+    timezone,
+    timedelta,
+)
+
+from jwt import (
+    encode,
+    decode,
+)
+
+from grocery.config import JWTConfig
+
+
+def encode_jwt(
+    payload: dict[str, Any],
+    key: str = JWTConfig().PRIVATE_KEY_PATH.read_text(),
+    algorithm: str = JWTConfig().ALGORITHM,
+    exp: timedelta = timedelta(minutes=JWTConfig().ACCESS_TOKEN_EXP_MINUTES)
+) -> str:
+    current_time = datetime.now(timezone.utc)
+    payload = payload | {
+        "iat": current_time,
+        "exp": current_time + exp
+    }
+    return encode(
+        payload=payload,
+        key=key,
+        algorithm=algorithm
+    )
+
+
+def decode_jwt(
+    access_token: str, 
+    key: str = JWTConfig().PUBLIC_KEY_PATH.read_text(),
+    algorithm: str = JWTConfig().ALGORITHM
+) -> Any:
+    return decode(
+        jwt=access_token,
+        key=key,
+        algorithms=[algorithm]
+    )
---
+++ grocery/utils/password.py
@@ -0,0 +1,15 @@
+import bcrypt
+
+
+def hash_password(password: str) -> bytes:
+    return bcrypt.hashpw(
+        password.encode("utf-8"),
+        bcrypt.gensalt()
+    )
+
+
+def cmp_password(password: str, hashed_password: bytes) -> bool:
+    return bcrypt.checkpw(
+        password.encode("utf-8"),
+        hashed_password
+    )
