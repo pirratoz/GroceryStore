a7afc9110d85ec10950ccd8d9fce17cdae1f1440
PirraToZ <pirratoz@gmail.com>
2024-07-17 20:07:02 +0300

    feat(): add application initialization

--- main.py
+++ main.py
@@ -1,2 +1,33 @@
-if __name__ == "__main__":
+from fastapi import FastAPI
+from uvicorn import run as uvicorn_run
+
+from grocery.dependencies import (
+    Session,
+    SessionReadOnly,
+    DatabaseConnector,
+    Auth,
+)
+
+
+def setup_dependency(app: FastAPI, db: DatabaseConnector) -> None:
+    app.dependency_overrides[Session] = db.get_session
+    app.dependency_overrides[SessionReadOnly] = db.get_session_read_only
+    app.dependency_overrides[Auth] = Auth.auth
+
+
+def include_endpoints(app: FastAPI) -> None:
     ...
+
+
+def main() -> None:
+    app = FastAPI()
+    postgresql = DatabaseConnector()
+
+    setup_dependency(app, postgresql)
+    include_endpoints(app)
+    
+    uvicorn_run(app)
+
+
+if __name__ == "__main__":
+    main()
