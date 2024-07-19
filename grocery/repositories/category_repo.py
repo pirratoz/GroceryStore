6c61f933ae9c2b879d7edc4cbab60cccbe039584
PirraToZ <pirratoz@gmail.com>
2024-07-19 18:32:53 +0300

    feat(): add methods in repositories

--- grocery/repositories/category_repo.py
+++ grocery/repositories/category_repo.py
@@ -1,5 +1,44 @@
+from uuid import UUID
+
+import sqlalchemy as sa
+
 from grocery.repositories.base_repo import BaseRepository
+from grocery.models import (
+    Category,
+    SubCategory,
+)
+from grocery.dto import CategoryDto
 
 
 class CategoryRepository(BaseRepository):
-    ...
+    async def get_categories_by_offset(self, limit: int, offset: int) -> list[Category]:
+        stmt = sa.select(Category).offset(offset=offset).limit(limit=limit).order_by(Category.id.desc())
+        categories = (await self.session.execute(stmt)).scalars().all()
+        return CategoryDto.many_from_orm(categories)
+
+    async def get_category_by_slug(self, slug: str) -> Category | None:
+        stmt = sa.select(Category).where(Category.slug == slug)
+        category = (await self.session.execute(stmt)).scalar_one_or_none()
+        return CategoryDto.one_from_orm(category)
+
+    async def get_count_record(self) -> int:
+        stmt = sa.select(sa.func.count()).select_from(Category)
+        total = (await self.session.execute(stmt)).scalar_one()
+        return total
+    
+    async def create(
+        self,
+        *,
+        title: str,
+        slug: str,
+        image_id: UUID,
+    ) -> CategoryDto:
+        category = Category(
+            title=title,
+            slug=slug,
+            image_id=str(image_id)
+        )
+        self.session.add(category)
+        await self.session.flush()
+        return CategoryDto.one_from_orm(category)
+    
--- grocery/repositories/image_repo.py
+++ grocery/repositories/image_repo.py
@@ -1,5 +1,20 @@
+from uuid import UUID
+
+import sqlalchemy as sa
+
 from grocery.repositories.base_repo import BaseRepository
+from grocery.models import Image
+from grocery.dto import ImageDto
 
 
 class ImageRepository(BaseRepository):
-    ...
+    async def create(self, *, filename: str) -> ImageDto:
+        image = Image(filename=filename)
+        self.session.add(image)
+        await self.session.flush()
+        return ImageDto.one_from_orm(image)
+    
+    async def get_by_id(self, *, id: UUID) -> ImageDto | None:
+        stmt = sa.select(Image).where(Image.id == id)
+        image = (await self.session.execute(stmt)).scalar_one_or_none()
+        return ImageDto.one_from_orm(image)
