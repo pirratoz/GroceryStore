860db876754dfeac5c4dcf7357a5b6168cf8c208
PirraToZ <pirratoz@gmail.com>
2024-07-20 13:35:16 +0300

    refactor(): output info

--- first_part.py
+++ first_part.py
@@ -1,9 +1,13 @@
 def main(n: int) -> None:
-    string = ""
+    string, length = "", 0
     for i in range(1, n + 1):
+        if n < length + i:
+            string += str(i) * (n - length)
+            break
         string += str(i) * i
+        length += i
     print(string)
 
 
 if __name__ == "__main__":
-    main(n=5)
+    main(n=0)
